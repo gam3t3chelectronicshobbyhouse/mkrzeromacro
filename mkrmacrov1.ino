69d940f249cc2553e350264184608f034600f24c
gam3t3chelectronicshobbyhouse <gam3t3ch@gmail.com>
2021-06-27 18:33:09 -0700

    Update mkrmacrov1.ino

--- mkrmacrov1.ino
+++ mkrmacrov1.ino
@@ -1,3 +1,10 @@
+// this code is also based on another persons work I am not the sole creator of this code here but unable to find the author to credit them here.
+//I will look more in my history to find out who it was but if you find out please email me at gam3t3ch@gmail.com so I can credit them on this.
+//my appologies I was unsure of how far I was going to go with mkrzero macro keyboard.
+//-Gam3t3ch
+
+//slowly working on a lcd screen addon will update more once I get another board together with proper setup with hardware.
+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Keyboard.h>
